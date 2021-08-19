5f05acc784057328440507e828d6287ed41279bd
dongdonghyeok <jay1415@naver.com>
2021-08-19 17:09:21 +0900

    21/08/18 commit 2 projectapp  articleapp 연결후 detail.html 설정

--- articleapp/models.py
+++ articleapp/models.py
@@ -11,7 +11,6 @@ class Article(models.Model):
     project = models.ForeignKey(Project, on_delete=models.SET_NULL,
                                related_name='article', null=True)
 
-
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     content = models.TextField(null=True)
